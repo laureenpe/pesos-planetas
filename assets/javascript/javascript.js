9805bb71243f5d552cf0361f36041554c02060ff
Laureen Pelaez Isaza <Laureen Pelaez Isaza>
2017-05-08 18:58:28 -0300

    calculo function

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,28 @@
+
+/*function calculo() {
+    var peso, planetaMarte, planetaJupiter, peso_final, nombre; //variables necesitadas para el calculo
+    peso = document.getElementById("peso").value; //recogemos los datos mediante el id
+    planetaMarte = document.getElementById("marte").value;
+    planetaJupiter = document.getElementById("jupiter").value;
+
+    var g_tierra = 9.8;
+    var g_marte = 3.7;
+    var g_jupiter = 24.8;
+    
+    if (planetaMarte) {
+        peso_final = peso * g_marte / g_tierra;
+       // nombre = "Marte";
+    }
+    else if (planetaJupiter) {
+        peso_final = peso * g_jupiter / g_tierra;
+       // nombre = "Jupiter";
+    }
+    else {
+        peso_final = 1000000;
+      //  nombre = "Chile";
+    }
+    peso_final = parseInt(peso_final);
+   // document.getElementById("nombre").innerHTML = "peso_final";
+   console.log("Tu peso en " + nombre + " es : " + peso_final + " kilos"); //it should work
+}
+calculo()*/
