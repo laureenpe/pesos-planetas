db4b3979e8bef2a2de074c25bc40ecbd5dca76da
“Laura <laureen.pelaez.isaza@gmail.com>
2017-05-10 21:32:01 -0300

    change vars and conditions

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,28 +1,22 @@
-
-/*function calculo() {
-    var peso, planetaMarte, planetaJupiter, peso_final, nombre; //variables necesitadas para el calculo
+function calculo() {
+    var peso, planeta, peso_final, nombre; //variables necesitadas para el calculo
     peso = document.getElementById("peso").value; //recogemos los datos mediante el id
-    planetaMarte = document.getElementById("marte").value;
-    planetaJupiter = document.getElementById("jupiter").value;
+    planeta = document.getElementById("planetas").value;
+
+    console.log(planeta);
 
     var g_tierra = 9.8;
     var g_marte = 3.7;
     var g_jupiter = 24.8;
-    
-    if (planetaMarte) {
+
+    if (planeta == "Marte") {
         peso_final = peso * g_marte / g_tierra;
-       // nombre = "Marte";
-    }
-    else if (planetaJupiter) {
+    } else if (planeta == "Jupiter") {
         peso_final = peso * g_jupiter / g_tierra;
-       // nombre = "Jupiter";
-    }
-    else {
-        peso_final = 1000000;
-      //  nombre = "Chile";
     }
     peso_final = parseInt(peso_final);
-   // document.getElementById("nombre").innerHTML = "peso_final";
-   console.log("Tu peso en " + nombre + " es : " + peso_final + " kilos"); //it should work
+    // document.getElementById("nombre").innerHTML = "peso_final";
+
+    console.log("Tu peso en " + planeta + " es : " + peso_final + " kilos"); //it should work
+
 }
-calculo()*/
